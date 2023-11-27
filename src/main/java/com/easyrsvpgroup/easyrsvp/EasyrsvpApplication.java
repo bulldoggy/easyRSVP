a832cccfb360ba09fb0272c527b94f845d29a46f
bulldoggy <ivanlim97@hotmail.com>
2023-11-27 23:51:11 +0800

    first commit with initializing files

---
+++ src/main/java/com/easyrsvpgroup/easyrsvp/EasyrsvpApplication.java
@@ -0,0 +1,13 @@
+package com.easyrsvpgroup.easyrsvp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyrsvpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyrsvpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/easyrsvpgroup/easyrsvp/EasyrsvpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easyrsvpgroup.easyrsvp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyrsvpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
